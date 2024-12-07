3a1ad580017037e7e4f6434fd4f9fd0357d2f207
Lukas <lburg5534@gmail.com>
2024-12-07 14:17:25 +0100

    add line extraction

--- code/file_scraper.py
+++ code/file_scraper.py
@@ -1,12 +1,32 @@
-def chunk_csv_file(file_path, chunk_size):
-    """
-    Chunk a csv file into smaller files of size chunk_size
-    """
-    with open(file_path, 'r') as f:
-        reader = csv.reader(f)
-        header = next(reader)
-        for i, chunk in enumerate(chunked(reader, chunk_size)):
-            with open(f'{file_path}_{i}.csv', 'w') as chunk_file:
-                writer = csv.writer(chunk_file)
-                writer.writerow(header)
-                writer.writerows(chunk)
+class Document:
+    def __init__(self, text, season, episode):
+        self.text = text
+        self.season = season
+        self.episode = episode
+
+def extract_lines_with_two_speaker_changes(df):
+    result = []
+    current_group = []
+    previous_speaker = None
+    speaker_changes = 0
+
+    for index, row in df.iterrows():
+        speaker = row['Character']
+        line = row['Line']
+
+        if previous_speaker and previous_speaker != speaker:
+            speaker_changes += 1
+
+        current_group.append(f"{speaker}: {line}")
+
+        if speaker_changes == 4:
+            result.append(" ".join(current_group))
+            current_group = []
+            speaker_changes = 0
+
+        previous_speaker = speaker
+
+    if current_group:
+        result.append(" ".join(current_group))
+
+    return result
--- code/main.py
+++ code/main.py
@@ -1,7 +1,18 @@
 import postgres_handler
 import mongo_handler
+import pandas as pd
+from file_scraper import extract_lines_with_two_speaker_changes
+import ollama
 
 if __name__ == '__main__':
     postgres_handler = postgres_handler.PostgresHandler()
-    #mongo_handler = mongo_handler.MongoHandler()
+    mongo_handler = mongo_handler.MongoHandler()
+
+    data = pd.read_csv("the-office_lines.csv")
+    test = extract_lines_with_two_speaker_changes(data.iloc[0:10])
+
+    for dialogs in test:
+        response = ollama.embeddings(model="mxbai-embed-large", prompt=dialogs)
+        embedding = response["embedding"]
+
     pass
